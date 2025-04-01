94b224d99120dd8079bcccfdfda22a22dd90f6cd
Tyler Dean <tylerwilliamdean@icloud.com>
2025-04-01 13:45:51 -0400

    changed it to a script and added code to hopefully finish the project

--- functional/functional.exs
+++ functional/functional.exs
@@ -1,17 +1,4 @@
 defmodule Functional do
-  use Application
-
-  def start(_type, _args) do
-    Functional.main()
-    Supervisor.start_link([], strategy: :one_for_one)
-  end
-
-  def main do
-    #500 words of Lorem ipsum for analysis.
-    lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu purus luctus, lobortis velit vitae, finibus mauris. Mauris nec mi consequat, luctus orci ac, facilisis turpis. Morbi sed malesuada ipsum. Aenean posuere venenatis orci quis egestas. Nunc ac vulputate ante, pulvinar scelerisque ex. Mauris efficitur, mauris nec placerat commodo, quam est facilisis nisi, id porta elit est ut nisi. In sagittis nulla sit amet leo iaculis, et aliquam lacus dapibus. Maecenas semper tellus sed magna porta interdum. Integer gravida ac eros id venenatis. Curabitur nec elit ex. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc finibus lorem leo. Sed laoreet dapibus aliquam. Aliquam consectetur ipsum vitae magna scelerisque dapibus. Mauris ultricies tincidunt pretium. Nunc molestie mollis odio. Praesent suscipit mattis consectetur. Proin diam nisl, efficitur a laoreet sit amet, mollis malesuada risus. Cras et est ut libero varius cursus et nec felis. Quisque tincidunt aliquet ligula, vitae interdum velit efficitur quis. Sed porta eros at vulputate consequat. Nulla lacinia fermentum nisi quis imperdiet. Proin nec nibh tempus, dictum ex sed, vehicula sem. Morbi tristique arcu et tellus semper mollis. Donec interdum est a iaculis fringilla. Duis egestas tellus vel lectus laoreet, sed suscipit lorem fermentum. Suspendisse id nisl eu erat luctus ultrices ut sed est. Nulla sodales ante in varius vulputate. Nullam odio nulla, vulputate at euismod et, placerat ut purus. Nulla rutrum nisl vel ante laoreet, nec hendrerit urna suscipit. Praesent bibendum, lectus id tincidunt congue, velit mauris blandit lectus, vel interdum leo ex id diam. Nullam dignissim nulla at neque fermentum fermentum. In venenatis est ut odio accumsan, a rhoncus mauris ornare. Duis tellus risus, lacinia ac mauris id, fermentum convallis tellus. Phasellus ante orci, lacinia in tellus sit amet, luctus fermentum risus. Nulla nec hendrerit nisi. Nullam maximus ac velit vitae convallis. Quisque nec lectus ac diam sollicitudin lobortis in et eros. Nulla fermentum aliquet risus. Fusce luctus erat non augue vestibulum lobortis. Aliquam ac quam sem. Sed vitae turpis nec mauris laoreet iaculis in vel metus. Aenean scelerisque tincidunt ultricies. Suspendisse consequat pharetra fringilla. Vestibulum vel ipsum vitae quam molestie laoreet. In rhoncus, ante eget eleifend congue, nisl sem sodales felis, sed facilisis tellus purus eget erat. Ut ut dolor lacinia, cursus ipsum ut, convallis arcu. Sed consectetur iaculis felis, eget scelerisque sem rhoncus eget. Nunc egestas magna condimentum felis semper convallis. Fusce eu cursus odio. Vivamus posuere dictum sapien quis interdum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer molestie turpis quis dolor gravida vehicula. Nunc scelerisque sapien bibendum nunc lacinia mattis. Maecenas non consequat magna, sed interdum nulla. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse vel enim eu dui consectetur ultrices vel posuere nisi. Cras hendrerit, eros nec feugiat dapibus, mauris nisi maximus erat, a laoreet diam elit nec massa. Proin non elit dictum, egestas tellus quis, ullamcorper turpis. Sed in lorem imperdiet, commodo massa ut."
-    countWordsAndSentences(lorem)
-  end
-
   def countWordsAndSentences(text) do
 
     sentencesList = splitIntoSentences(text)
@@ -21,17 +8,22 @@ defmodule Functional do
     IO.puts("There are " <> Integer.to_string(numOfSentences) <> " sentences. The longest one is: ")
 
     #Find and print the longest sentence
+    IO.inspect(longestString(sentencesList))
 
     #Analyze the word counts,
-    totalWordCount = length(splitWords(text))
+    wordList = splitWords(text)
+    totalWordCount = length(wordList)
     IO.puts("There are " <> Integer.to_string(totalWordCount) <> " words in this text sample. The longest one is: ")
 
     #Find the longest word
+    IO.inspect(longestString(wordList))
 
     wordFreqMap = mapWords(text)
     uniqueWordCount = length(Map.keys(wordFreqMap))
-    IO.puts("There are " <> Integer.to_string(uniqueWordCount) <> " unique words used. The most common are: ")
-    IO.inspect(topTen(wordFreqMap))
+    IO.puts("There are " <> Integer.to_string(uniqueWordCount) <> " unique words used. The most common words and their respective counts are: ")
+    topTen = topTen(wordFreqMap)
+    IO.inspect(topTen)
+    IO.inspect(listOfValuesFromKeys(topTen, wordFreqMap))
 
     #I want to show the words and their count.
 
@@ -39,7 +31,7 @@ defmodule Functional do
     :done
   end
 
-  def splitIntoSentences(text, sentences \\ [], currentWord \\ <<>>) do
+  def splitIntoSentences(text, sentences \\ [], currentSentence \\ <<>>) do
     remChars = byte_size(text)
     if remChars == 0 do
       sentences
@@ -48,11 +40,14 @@ defmodule Functional do
 
       {newSentences, word} = case currentChar do
         c when c in ~c".!?" ->
-          newSentences = sentences ++ [currentWord]
+
+          trimmedSentence = String.trim(currentSentence, " ")
+          newSentences = sentences ++ [trimmedSentence]
           {newSentences, <<>>}
         _ ->
-          {sentences, currentWord <> <<currentChar::8>> }
+          {sentences, currentSentence <> <<currentChar::8>> }
       end
+
       #IO.inspect({remText, newSentences, word}) #For debugging purposes
       splitIntoSentences(remText, newSentences, word)
     end
@@ -158,7 +153,7 @@ defmodule Functional do
         modifiedList = List.replace_at(list, i+1, key)
         insertIntoOrderedList(modifiedList, i+1, originalKey, dict)
       else
-      #value > Enum.at(list, i]
+      #value > Enum.at(list, i)
         if (i == 0) do
           originalKey = Enum.at(list, i)
           modifiedList = List.replace_at(list, i, key)
@@ -170,4 +165,29 @@ defmodule Functional do
       end
     end
   end
+
+  def longestString(list, i \\ 0, longestIndex \\ 0) do
+    if (i >= length(list)) do
+      Enum.at(list, longestIndex)
+    else
+      if (byte_size(Enum.at(list, i)) > byte_size(Enum.at(list, longestIndex))) do
+        longestString(list, i+1, i)
+      else
+        longestString(list, i+1, longestIndex)
+      end
+    end
+  end
+
+  def listOfValuesFromKeys(keys, dict, valueList \\ [], i \\ 0) do
+    if (i < length(keys)) do
+      value = Map.get(dict, Enum.at(keys, i))
+      valueList = List.insert_at(valueList, length(valueList), Integer.to_string(value))
+      listOfValuesFromKeys(keys, dict, valueList, i+1)
+    else
+      valueList
+    end
+  end
 end
+
+lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu purus luctus, lobortis velit vitae, finibus mauris. Mauris nec mi consequat, luctus orci ac, facilisis turpis. Morbi sed malesuada ipsum. Aenean posuere venenatis orci quis egestas. Nunc ac vulputate ante, pulvinar scelerisque ex. Mauris efficitur, mauris nec placerat commodo, quam est facilisis nisi, id porta elit est ut nisi. In sagittis nulla sit amet leo iaculis, et aliquam lacus dapibus. Maecenas semper tellus sed magna porta interdum. Integer gravida ac eros id venenatis. Curabitur nec elit ex. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc finibus lorem leo. Sed laoreet dapibus aliquam. Aliquam consectetur ipsum vitae magna scelerisque dapibus. Mauris ultricies tincidunt pretium. Nunc molestie mollis odio. Praesent suscipit mattis consectetur. Proin diam nisl, efficitur a laoreet sit amet, mollis malesuada risus. Cras et est ut libero varius cursus et nec felis. Quisque tincidunt aliquet ligula, vitae interdum velit efficitur quis. Sed porta eros at vulputate consequat. Nulla lacinia fermentum nisi quis imperdiet. Proin nec nibh tempus, dictum ex sed, vehicula sem. Morbi tristique arcu et tellus semper mollis. Donec interdum est a iaculis fringilla. Duis egestas tellus vel lectus laoreet, sed suscipit lorem fermentum. Suspendisse id nisl eu erat luctus ultrices ut sed est. Nulla sodales ante in varius vulputate. Nullam odio nulla, vulputate at euismod et, placerat ut purus. Nulla rutrum nisl vel ante laoreet, nec hendrerit urna suscipit. Praesent bibendum, lectus id tincidunt congue, velit mauris blandit lectus, vel interdum leo ex id diam. Nullam dignissim nulla at neque fermentum fermentum. In venenatis est ut odio accumsan, a rhoncus mauris ornare. Duis tellus risus, lacinia ac mauris id, fermentum convallis tellus. Phasellus ante orci, lacinia in tellus sit amet, luctus fermentum risus. Nulla nec hendrerit nisi. Nullam maximus ac velit vitae convallis. Quisque nec lectus ac diam sollicitudin lobortis in et eros. Nulla fermentum aliquet risus. Fusce luctus erat non augue vestibulum lobortis. Aliquam ac quam sem. Sed vitae turpis nec mauris laoreet iaculis in vel metus. Aenean scelerisque tincidunt ultricies. Suspendisse consequat pharetra fringilla. Vestibulum vel ipsum vitae quam molestie laoreet. In rhoncus, ante eget eleifend congue, nisl sem sodales felis, sed facilisis tellus purus eget erat. Ut ut dolor lacinia, cursus ipsum ut, convallis arcu. Sed consectetur iaculis felis, eget scelerisque sem rhoncus eget. Nunc egestas magna condimentum felis semper convallis. Fusce eu cursus odio. Vivamus posuere dictum sapien quis interdum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer molestie turpis quis dolor gravida vehicula. Nunc scelerisque sapien bibendum nunc lacinia mattis. Maecenas non consequat magna, sed interdum nulla. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse vel enim eu dui consectetur ultrices vel posuere nisi. Cras hendrerit, eros nec feugiat dapibus, mauris nisi maximus erat, a laoreet diam elit nec massa. Proin non elit dictum, egestas tellus quis, ullamcorper turpis. Sed in lorem imperdiet, commodo massa ut."
+Functional.countWordsAndSentences(lorem)
--- functional/test/functional_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule FunctionalTest do
-  use ExUnit.Case
-  doctest Functional
-
-  test "greets the world" do
-    assert Functional.hello() == :world
-  end
-end
