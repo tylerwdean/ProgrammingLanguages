4d8b275d30db71f66c0bcb7e4d52ed3420cf3408
Tyler Dean <tylerwilliamdean@icloud.com>
2025-04-09 14:43:34 -0400

    Added timer to parallel program

--- parallel/src/main.rs
+++ parallel/src/main.rs
@@ -2,26 +2,30 @@ use rand::prelude::*;
 use std::thread;
 use std::sync::Arc;
 use std::sync::mpsc;
+use std::time::Instant;
 
 fn main() {
-    let v = initialize_vector();
-    let thread_count = 10;
+    let thread_count = 1;
+    let v_length: usize = 10_000_000;
+    let v = initialize_vector(v_length);
     let v_arc = Arc::new(v);    //for sharing v as read only
 
     let (tx, rx) = mpsc::channel();
     let mut handles = vec![];
 
+    println!("Starting timer now!");
+    let start = Instant::now();
 
     for i in 0..thread_count {
         let tx_clone = tx.clone();
         let v_reference = v_arc.clone();
 
         handles.push(thread::spawn(move || {
-            println!("Starting thread {}", i);
-            let range = 1_000_000/thread_count;
+            //println!("Starting thread {}", i);
+            let range: usize = v_length/thread_count;
             let sum = sum_vector(&*v_reference, i*range, (i+1)*range);
             tx_clone.send(sum).unwrap();
-            println!("Closing thread {}", i);
+            //println!("Closing thread {}", i);
         }))
     }
 
@@ -30,9 +34,9 @@ fn main() {
     let mut sum: i64 = 0;
     for received in rx {
         sum += received;
-        println!("Adding results together, sum is: {}", sum);
+        //println!("Adding results together, sum is: {}", sum);
     }
-    println!("Program completed");
+    println!("Program completed!\nSum: {}\nSummation time: {:?}", sum, start.elapsed());
 }
 
 fn sum_vector(v: &Vec<i32>, start: usize, end: usize) -> i64{
@@ -46,15 +50,16 @@ fn sum_vector(v: &Vec<i32>, start: usize, end: usize) -> i64{
     return sum;
 }
 
-fn initialize_vector() -> Vec<i32>{
+fn initialize_vector(v_length: usize) -> Vec<i32>{
 
     println!("Starting vector initialization");
-    let mut v = vec![];
+    let mut v = vec![0; v_length];
+    println!("V has capactity {}", v.capacity());
     let mut rng = rand::rng();
 
-    for i in 0..1_000_000 {
+    for i in 0..(v_length as i32) {
         //v.push((rng.random::<i32>() % 1_000_000).abs());  //this generates a random value array, which has different results each time
-        v.push(i)   //this generates the same value each time, but demonstrates that the sum from threads are different
+        v[i as usize] = i;   //this generates the same value each time, but demonstrates that the sum from threads are different
     }
     v.shuffle(&mut rng);    //makes it randomly distributed
     println!("Vector initialized");
